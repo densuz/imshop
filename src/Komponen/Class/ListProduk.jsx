bfc5e8f314a740ddc360a97574ea4a943c983f06
densuz <densuz97@gmail.com>
2020-07-21 14:06:31 +0700

    Create Komponen RowProduk & ListProduk

---
+++ src/Komponen/Class/ListProduk.jsx
@@ -0,0 +1,119 @@
+import React, { PureComponent } from 'react'
+import axios from 'axios'
+import qs from 'querystring'
+import { Table, Button, Container, NavLink, Alert } from 'reactstrap'
+import { Link } from 'react-router-dom'
+import { type } from 'os'
+
+const api = 'http://localhost:3001'
+
+class ListComp extends PureComponent {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            t_barang: [],
+            response: '',
+            display: 'none'
+
+        }
+    }
+
+    componentDidMount() {
+        axios.get(api + '/tampil').then(res => {
+            this.setState({
+                barang: res.data.values
+            })
+        })
+    }
+
+    Deletebarang = (kode_barang) => {
+        const { t_barang } = this.state
+        const data = qs.stringify({
+            kode_barang: kode_barang
+        })
+
+        axios.delete(api + '/hapus',
+            {
+                data: data,
+                headers: { 'Content-type': 'application/x-www-form-urlencoded' }
+            }
+        ).then(json => {
+            if (json.data.status === 200) {
+                this.setState({
+                    response: json.data.values,
+                    t_barang: t_barang.filter(t_barang => t_barang.kode_barang !== kode_barang),
+                    display: 'block'
+                })
+                this.props.history.push('/barang')
+            } else {
+                this.setState({
+                    response: json.data.values,
+                    display: 'block'
+                })
+                
+            }
+        })
+    }
+
+    render() {
+        return (
+            <Container>
+                <h2> Data Barang</h2>
+                <Alert color="success" style={{ display: this.state.display }}>
+                    {this.state.response}
+                </Alert>
+                <NavLink href="/barang/tambah"><Button color="success">Tambah Data</Button></NavLink>
+                <hr />
+                <Table className="table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Kode Barang</th>
+                            <th>Nama Barang</th>
+                            <th>Harga(Rp. )</th>
+                            <th>Jumlah</th>
+                            <th>Gambar</th>
+                            <th>Satuan</th>
+
+                            <th>Opsi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.t_barang.map(t_barang =>
+                            <tr key={t_barang.kode_barang}>
+                                <td>{t_barang.nama_barang}</td>
+                                <td>{t_barang.harga}</td>
+                                <td>{t_barang.jumlah_barang}</td>
+                                <td>{t_barang.gambar_barang}</td>
+                                <td>{t_barang.satuan}</td>
+                                <td>
+                                    <Link to=
+                                        {
+                                            {
+                                                pathname: '/barang/edit',
+                                                state: {
+                                                    kode_barang: t_barang.kode_barang,
+                                                    nama_barang: t_barang.nama_barang,
+                                                    harga: t_barang.harga,
+                                                    jumlah_barang: t_barang.jumlah_barang,
+                                                    gambar_barang: t_barang.gambar_barang,
+                                                    satuan: t_barang.satuan,
+                                                }
+                                            }
+                                        }>
+                                        <Button> Edit </Button>
+
+                                    </Link>
+                                    <span> </span>
+                                        <Button onClick={() => this.Deletebarang(t_barang.kode_barang)} color="danger"> Delete </Button>
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+
+                </Table>
+            </Container>
+        )
+    }
+}
+export default ListComp;
---
+++ src/Komponen/Fungsional/RowProduct.jsx
@@ -0,0 +1,87 @@
+import React, { Component, useReducer } from 'react';
+import {
+    Col, Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+import { useContext } from 'react';
+import { CartContext } from '../../CartContex';
+import { Link } from 'react-router-dom'
+
+
+const initialState = {
+    jumlah: 1,
+    hargasatuan: 1000,
+    hargatotal: 0
+}
+
+const reducer = (state, action) => {
+
+
+
+    switch (action.type) {
+        case 'tambah': return {
+            ...state,
+            jumlah: state.jumlah + 1,
+            hargatotal: state.hargasatuan + (state.hargasatuan * state.jumlah)
+        }
+        case 'kurangi': return {
+            ...state,
+            jumlah: state.jumlah - 1,
+            hargatotal: (state.hargasatuan * state.jumlah) - state.hargasatuan
+        }
+        default:
+            return state
+
+    }
+}
+
+function Rowproduct(props) {
+    
+    const [count] = useReducer(reducer, initialState)
+    const { value, setValue } = useContext(CartContext)
+
+
+
+    return (
+        <Col>
+            <Card>
+                <CardImg top width="100%" src="https://placeimg.com/640/480/any" alt="Card image cap" />
+                <CardBody>
+                    <CardTitle>{props.item}</CardTitle>
+                    <CardSubtitle>Rp. {count.hargasatuan}</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button  color="danger">
+                        <Link to={
+                            {
+                                pathname: '/detailproduk',
+                            }
+                        }>Detail</Link>
+                    </Button>
+                    <span> | </span>
+                    <Button onClick={() => setValue(value + 1)} color="success">Tambahkan Keranjang</Button>
+                </CardBody>
+            </Card>
+            <hr/>
+            <CardImg top width="100%" src="https://placeimg.com/640/480/any" alt="Card image cap" />
+                <CardBody>
+                    <CardTitle>{props.item}</CardTitle>
+                    <CardSubtitle>Rp. {count.hargasatuan}</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button  color="danger">
+                        <Link to={
+                            {
+                                pathname: '/detailproduk',
+                            }
+                        }>Detail</Link>
+                    </Button>
+                    <span> | </span>
+                    <Button onClick={() => setValue(value + 1)} color="success">Tambahkan Keranjang</Button>
+                </CardBody>
+        </Col>
+        
+
+    );
+}
+
+
+export default Rowproduct;
